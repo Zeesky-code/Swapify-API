5c62677db09beeb2112692e49149bef0914f6c45
Zainab Lawal <zainablaw1012@gmail.com>
2023-01-06 18:06:02 +0300

    Restructured project to contain only API

--- client/src/components/MenuData.js
+++
@@ -1,8 +0,0 @@
-export const MenuData = [
-    {
-        title: "Try it Now",
-        url: "#",
-        cName: "nav-links"
-    }
-]
-
--- client/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import App from "./App.js";  
-
-import { createRoot } from 'react-dom/client';
-import 'font-awesome/css/font-awesome.min.css';
-
-const container = document.getElementById('root');
-const root = createRoot(container); // createRoot(container!) if you use TypeScript
-root.render(<App />);
